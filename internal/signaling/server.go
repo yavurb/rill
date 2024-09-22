ebe682f305661c78c3b2cd60f92f70e79aa608cd
Yurb <yavurb@gmail.com>
2024-09-22 13:20:00 -0400

    chore: reduce number of stun servers

--- internal/signaling/server.go
+++ internal/signaling/server.go
@@ -201,8 +201,6 @@ func HandleViewer(viewerSDPChan string, track *webrtc.TrackLocalStaticRTP, viewe
 		{URLs: []string{"stun:stun.l.google.com:19302"}},
 		{URLs: []string{"stun:stun1.l.google.com:19302"}},
 		{URLs: []string{"stun:stun2.l.google.com:19302"}},
-		{URLs: []string{"stun:stun3.l.google.com:19302"}},
-		{URLs: []string{"stun:stun4.l.google.com:19302"}},
 	}
 	peerConnectionConfig := webrtc.Configuration{
 		ICEServers: ICEServers,
